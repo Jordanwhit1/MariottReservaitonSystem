b410321b11a368e7095fe8359cb84a9cc72a8b47
Jordan Nichole Whitaker <Jordanwhit1@aol.com>
2022-10-17 11:34:14 -0400

    Update Reservations.java

---
+++ Reservations.java
@@ -0,0 +1,26 @@
+//import java.util.Scanner;
+public class Reservations  {
+    //This is the class for everything reservations
+    String fname;
+    String lname;
+    int roomType;
+    int roomNumber;
+    int numDaysStay;
+
+    public Reservations(String firstname, String lastname, int roomSelection, int numDays, int roomNum) {
+        fname = firstname;
+        lname = lastname;
+        roomType = roomSelection;
+        numDaysStay = numDays;
+        roomNumber = roomNum;
+    }
+
+
+public static void main(String[] args){
+
+
+}
+
+
+}
+
